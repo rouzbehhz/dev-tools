e1866b898af4dae11371bab0610d7e64f32e2a4b
Rouzbeh Hz <rouzbehhz@gmail.com>
2018-07-01 22:02:56 -0400

    Delimiter for bin to ascii

--- dev-tools.js
+++ dev-tools.js
@@ -1,7 +1,7 @@
 var devTools = {
     strToBin: function (input) {
         var output = '';
-        input = input.replace(/’/g,'\'').split('')
+        input = input.split('')
         for ( var i = 0; i < input.length; i++ ) {
             var singleChar = Array(input[i].charCodeAt(0).toString(2) + ',')
             function addZero() {
@@ -16,11 +16,19 @@ var devTools = {
             checkLength()
             output += singleChar
         }
-        return Array(output)
+        output = output.split(',')
+        output.pop()
+        output = output.toString().replace(/,/g,' ')
+        return output
     },
-    binToAscii: function (input) {
+    binToAscii: function (input, delimiter) {
         var output = '';
-        input = input.split(' ')
+        if (delimiter) {
+            input = input.split(delimiter)
+        }
+        else {
+            input = input.split(' ')
+        }
         for ( var i = 0; i < input.length; i++ ) {
             var multiplier = 0,
                 addIt = 0;
@@ -32,16 +40,19 @@ var devTools = {
             }
             singleBin = addIt.split(" ").map(Number);
             singleBin = singleBin.reduce((a, b) => a + b, 0);
-            output += singleBin + ','
+            output += (singleBin + ',')
         }
-        return Array(output)
+        output = output.split(',')
+        output.pop()
+        output = output.toString().replace(/,/g,' ')
+        return output
     }
 }
 
 ////// Convert some string to binary.
-// var someStr = "Conver this into binary!"
-// console.log(devTools.strToBin(someStr))
+var someStr = "Convert this into binary!"
+console.log(devTools.strToBin(someStr))
 
 ////// Convert some binary to ASCII.
-// var someBin = '01000011 01101111 01101110 01110110 01100101 01110010 01110100 00100000 01101101 01100101 00100000 01110100 01101111 00100000 01100001 01110011 01100011 01101001 01101001 00100001'
-// console.log(devTools.binToAscii(someBin))
+var someBin = '01000011 01101111 01101110 01110110 01100101 01110010 01110100 00100000 01110100 01101000 01101001 01110011 00100000 01101001 01101110 01110100 01101111 00100000 01100010 01101001 01101110 01100001 01110010 01111001 00100001'
+console.log(devTools.binToAscii(someBin))

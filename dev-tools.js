722be4d8fadb5497691398912eb0b8c3d2f0b2c7
rouzbehhz <40371763+rouzbehhz@users.noreply.github.com>
2018-07-03 16:15:58 -0400

    String to ASCII & ASCII to string

--- dev-tools.js
+++ dev-tools.js
@@ -1,79 +1,105 @@
 var devTools = {
     strToBin: function (input) {
-        var output = '';
-        input = input.split('')
-        for ( var i = 0; i < input.length; i++ ) {
-            var singleChar = input[i].charCodeAt(0).toString(2) + ','
-            function addZero() {
-                singleChar = 0 + singleChar
-                checkLength();
-            }
-            function checkLength() {
-                if ( singleChar.length <= 8 ) {
-                    addZero();
-                }
-            }
-            checkLength()
-            output += singleChar
-        }
-        output = output.split(',')
-        output.pop()
-        output = output.toString().replace(/,/g,' ')
-        return output
+	var output = '';
+	    input = input.split('')
+	for ( var i = 0; i < input.length; i++ ) {
+	    var singleChar = input[i].charCodeAt(0).toString(2) + ','
+	    function addZero() {
+		singleChar = 0 + singleChar
+		checkLength();
+	    }
+	    function checkLength() {
+		if ( singleChar.length <= 8 ) {
+		    addZero();
+		}
+	    }
+	    checkLength()
+	    output += singleChar
+	}
+	output = output.split(',')
+	output.pop()
+	output = output.toString().replace(/,/g,' ')
+	return output
     },
     binToAscii: function (input, delimiter) {
-        this.output = '',
-        this.delimiter = delimiter;
-        if ( this.delimiter ) {
-            input = input.split(this.delimiter)
-        }
-        else {
-            input = input.match(/.{1,8}/g);
-        }
-        for ( var i = 0; i < input.length; i++ ) {
-            var multiplier = 0,
-                addIt = 0;
-            bin = input[i].split('').reverse()
-            for (var o = 0; o < bin.length; o++) {
-                powOf = Math.pow(2, multiplier)
-                addIt += bin[o] * powOf + ' '
-                multiplier++
-            }
-            singleBin = addIt.split(" ").map(Number);
-            singleBin = singleBin.reduce((a, b) => a + b, 0);
-            this.output += (singleBin + ',')
-        }
-        this.output = this.output.split(',')
-        this.output.pop()
-        this.output = this.output.toString().replace(/,/g,' ')
-        return this.output
-    },
-    binToText: function(input, delimiter) {
-            this.output = '',
-            this.arr = '',
-            this.delimiter = delimiter;
-
-        if ( this.delimiter ) {
-            this.arr = input.split(this.delimiter);
-        }
-        else {
-            this.arr = input.match(/.{1,8}/g);
-        }
-		for (var i = 0; i < this.arr.length; i++) {
-			this.output += String.fromCharCode(parseInt(this.arr[i], 2).toString(10));
-		}
-		return this.output;
+	var output = '';
+	if ( delimiter ) {
+	    input = input.split(delimiter)
 	}
+	else {
+	    input = input.match(/.{1,8}/g);
+	}
+	for ( var i = 0; i < input.length; i++ ) {
+	    var multiplier = 0,
+		addIt = 0;
+	    bin = input[i].split('').reverse()
+	    for (var o = 0; o < bin.length; o++) {
+		powOf = Math.pow(2, multiplier)
+		addIt += bin[o] * powOf + ' '
+		multiplier++
+	    }
+	    singleBin = addIt.split(" ").map(Number);
+	    singleBin = singleBin.reduce((a, b) => a + b, 0);
+	    output += (singleBin + ',')
+	}
+	output = output.split(',')
+	output.pop()
+	output = output.toString().replace(/,/g,' ')
+	return output
+    },
+    binToStr: function(input, delimiter) {
+	    var output = '',
+		arr = '';
+
+	if ( delimiter ) {
+	    arr = input.split(delimiter);
+	}
+	else {
+	    arr = input.match(/.{1,8}/g);
+	}
+	for (var i = 0; i < arr.length; i++) {
+	    output += String.fromCharCode(parseInt(arr[i], 2).toString(10));
+	}
+	return output;
+    },
+    asciiToStr: function(input, delimiter) {
+	var output = '';
+	if( delimiter ) {
+	    input = input.split(delimiter)
+	}
+	else {
+	    input = input.split(' ')
+	}
+	for (var i = 0; i < input.length; i++) {
+	    output += String.fromCharCode(input[i]);
+	}
+	return output;
+    },
+    strToAscii: function(input) {
+	var output = '';
+	input = input.split('')
+	for (var i = 0; i < input.length; i++) {
+	    output += input[i].charCodeAt(0) + ' ';
+	}
+	return output;
+    }
 }
 
+var someStr = "Convert this!"
+var someBin = '01000011 01101111 01101110 01110110 01100101 01110010 01110100 00100000 01110100 01101000 01101001 01110011 00100001'
+var someAscii = '67 111 110 118 101 114 116 32 116 104 105 115 33'
+
 ////// Convert some string to binary.
-var someStr = "Convert this into binary!"
 console.log(devTools.strToBin(someStr))
 
 ////// Convert some binary to ASCII.
-var someBin = '01000011 01101111 01101110 01110110 01100101 01110010 01110100 00100000 01110100 01101000 01101001 01110011 00100000 01101001 01101110 01110100 01101111 00100000 01100010 01101001 01101110 01100001 01110010 01111001 00100001'
 console.log(devTools.binToAscii(someBin, ' '))
 
-////// Convert some binary to Text.
-var someBin = '01000011 01101111 01101110 01110110 01100101 01110010 01110100 00100000 01110100 01101000 01101001 01110011 00100000 01101001 01101110 01110100 01101111 00100000 01100010 01101001 01101110 01100001 01110010 01111001 00100001'
-console.log(devTools.binToText(someBin, ' '))
+////// Convert some binary to string.
+console.log(devTools.binToStr(someBin, ' '))
+
+////// Convert some string to ASCII.
+console.log(devTools.strToAscii(someStr))
+
+////// Convert some ASCII to string.
+console.log(devTools.asciiToStr(someAscii, ' '))
